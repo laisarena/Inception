739742ce0ac1adceba2642fda076617be79c00e2
Lais Arena <laisarena@gmail.com>
2022-05-13 21:28:38 -0300

    Config wordpress and run php

---
+++ srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+WP_PATH=/var/www/lfrasson/wordpress
+WP_CONFIG=wp-config.php
+
+if [ ! -f "$WP_PATH/$WP_CONFIG" ]
+then
+	cd $WP_PATH
+	mv wp-config-sample.php $WP_CONFIG
+	sed -i "s/username_here/$MYSQL_USER/g" $WP_CONFIG 
+	sed -i "s/password_here/$MYSQL_PASSWORD/g" $WP_CONFIG 
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g" $WP_CONFIG 
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" $WP_CONFIG 
+fi
+
+exec "$@"
