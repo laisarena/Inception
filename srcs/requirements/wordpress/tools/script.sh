5da0d6bc5bf82537ebd8a481b741962e4b6132a6
Lais Arena <laisarena@gmail.com>
2022-05-16 22:48:51 -0300

    Fix mysql host on wp config

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -9,7 +9,7 @@ then
 	mv wp-config-sample.php $WP_CONFIG
 	sed -i "s/username_here/$MYSQL_USER/g" $WP_CONFIG 
 	sed -i "s/password_here/$MYSQL_PASSWORD/g" $WP_CONFIG 
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" $WP_CONFIG 
+	sed -i "s/localhost/$MYSQL_HOST/g" $WP_CONFIG 
 	sed -i "s/database_name_here/$MYSQL_DATABASE/g" $WP_CONFIG 
 fi
 
