b8b706ed5742ca9ba74cc1b2309b182efe7b83b9
Lais Arena <laisarena@gmail.com>
2022-05-19 19:04:43 -0300

    Change host on script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,9 +6,9 @@ if [ ! -d "$DATABASE_PATH" ]
 then
 	service mysql start;
 	mysql -u root --execute="CREATE DATABASE $MYSQL_DATABASE; \
-				 CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'; \
+				 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
 				 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; \
-				 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';";	
+				 GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';";	
 	service mysql stop;
 	
 fi
